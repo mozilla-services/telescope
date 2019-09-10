a07f395e736bf87b574f04099cc916409eb6045d
Mathieu Leplatre <mathieu@mozilla.com>
2019-09-10 16:48:04 +0200

    Move several parts to checks.remotesettings.utils (#21)
    * Move fetch_signed_resources to utils
    
    * Centralize kinto_http.Client with controlled timeout

---
+++ checks/remotesettings/utils.py
@@ -0,0 +1,122 @@
+import copy
+import os
+
+import backoff
+import kinto_http
+import requests
+from requests.adapters import TimeoutSauce
+
+
+REQUESTS_TIMEOUT_SECONDS = float(os.getenv("REQUESTS_TIMEOUT_SECONDS", 2))
+REQUESTS_MAX_RETRIES = int(os.getenv("REQUESTS_MAX_RETRIES", 4))
+
+
+retry_timeout = backoff.on_exception(
+    backoff.expo,
+    (requests.exceptions.Timeout, requests.exceptions.ConnectionError),
+    max_tries=REQUESTS_MAX_RETRIES,
+)
+
+
+class CustomTimeout(TimeoutSauce):
+    def __init__(self, *args, **kwargs):
+        if kwargs["connect"] is None:
+            kwargs["connect"] = REQUESTS_TIMEOUT_SECONDS
+        if kwargs["read"] is None:
+            kwargs["read"] = REQUESTS_TIMEOUT_SECONDS
+        super().__init__(*args, **kwargs)
+
+
+requests.adapters.TimeoutSauce = CustomTimeout
+
+
+class KintoClient(kinto_http.Client):
+    """
+    This Kinto client will retry the requests if they fail for timeout, and
+    if the server replies with a 5XX.
+    """
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("retry", REQUESTS_MAX_RETRIES)
+
+        auth = kwargs.get("auth")
+        if auth is not None:
+            _type = None
+            if " " in auth:
+                # eg, "Bearer ghruhgrwyhg"
+                _type, auth = auth.split(" ", 1)
+            auth = (
+                tuple(auth.split(":", 1))
+                if ":" in auth
+                else kinto_http.BearerTokenAuth(auth, type=_type)
+            )
+            kwargs["auth"] = auth
+
+        super().__init__(*args, **kwargs)
+
+    @retry_timeout
+    def server_info(self, *args, **kwargs):
+        return super().server_info(*args, **kwargs)
+
+    @retry_timeout
+    def get_collection(self, *args, **kwargs):
+        return super().get_collection(*args, **kwargs)
+
+    @retry_timeout
+    def get_records(self, *args, **kwargs):
+        return super().get_records(*args, **kwargs)
+
+    @retry_timeout
+    def get_records_timestamp(self, *args, **kwargs):
+        return super().get_records_timestamp(*args, **kwargs)
+
+
+def fetch_signed_resources(server_url, auth):
+    # List signed collection using capabilities.
+    client = KintoClient(server_url=server_url, auth=auth)
+    info = client.server_info()
+    try:
+        resources = info["capabilities"]["signer"]["resources"]
+    except KeyError:
+        raise ValueError("No signer capabilities found. Run on *writer* server!")
+
+    # Build the list of signed collections, source -> preview -> destination
+    # For most cases, configuration of signed resources is specified by bucket and
+    # does not contain any collection information.
+    resources_by_bid = {}
+    resources_by_cid = {}
+    preview_buckets = set()
+    for resource in resources:
+        bid = resource["destination"]["bucket"]
+        cid = resource["destination"]["collection"]
+        if resource["source"]["collection"] is not None:
+            resources_by_cid[(bid, cid)] = resource
+        else:
+            resources_by_bid[bid] = resource
+        if "preview" in resource:
+            preview_buckets.add(resource["preview"]["bucket"])
+
+    resources = []
+    monitored = client.get_records(
+        bucket="monitor", collection="changes", _sort="bucket,collection"
+    )
+    for entry in monitored:
+        bid = entry["bucket"]
+        cid = entry["collection"]
+
+        # Skip preview collections entries
+        if bid in preview_buckets:
+            continue
+
+        if (bid, cid) in resources_by_cid:
+            r = resources_by_cid[(bid, cid)]
+        elif bid in resources_by_bid:
+            r = copy.deepcopy(resources_by_bid[bid])
+            r["source"]["collection"] = r["destination"]["collection"] = cid
+            if "preview" in r:
+                r["preview"]["collection"] = cid
+        else:
+            raise ValueError(f"Unknown signed collection {bid}/{cid}")
+        resources.append(r)
+
+    return resources
---
+++ tests/checks/remotesettings/test_utils.py
@@ -0,0 +1,69 @@
+import responses
+
+from checks.remotesettings.utils import fetch_signed_resources
+
+
+def test_fetch_signed_resources(mocked_responses):
+    server_url = "http://fake.local/v1"
+    mocked_responses.add(
+        responses.GET,
+        server_url + "/",
+        json={
+            "capabilities": {
+                "signer": {
+                    "resources": [
+                        {
+                            "source": {"bucket": "blog-workspace", "collection": None},
+                            "preview": {"bucket": "blog-preview", "collection": None},
+                            "destination": {"bucket": "blog", "collection": None},
+                        },
+                        {
+                            "source": {
+                                "bucket": "security-workspace",
+                                "collection": "blocklist",
+                            },
+                            "destination": {
+                                "bucket": "security",
+                                "collection": "blocklist",
+                            },
+                        },
+                    ]
+                }
+            }
+        },
+    )
+    changes_url = server_url + "/buckets/monitor/collections/changes/records"
+    mocked_responses.add(
+        responses.GET,
+        changes_url,
+        json={
+            "data": [
+                {
+                    "id": "abc",
+                    "bucket": "blog",
+                    "collection": "articles",
+                    "last_modified": 42,
+                },
+                {
+                    "id": "def",
+                    "bucket": "security",
+                    "collection": "blocklist",
+                    "last_modified": 41,
+                },
+            ]
+        },
+    )
+
+    resources = fetch_signed_resources(server_url, auth="")
+
+    assert resources == [
+        {
+            "source": {"bucket": "blog-workspace", "collection": "articles"},
+            "preview": {"bucket": "blog-preview", "collection": "articles"},
+            "destination": {"bucket": "blog", "collection": "articles"},
+        },
+        {
+            "source": {"bucket": "security-workspace", "collection": "blocklist"},
+            "destination": {"bucket": "security", "collection": "blocklist"},
+        },
+    ]
