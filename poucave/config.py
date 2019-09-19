65e399983d0f4845d71650f1247c1958dfa89d62
Mathieu Leplatre <mathieu@mozilla.com>
2019-09-19 17:13:10 +0200

    Fix #7: Check for blocked pages (#64)
    * Fix #7: Check for blocked pages
    
    * Add bs4 to requirements
    
    * Add docstring
    
    * Add proper requirements

--- poucave/config.py
+++ poucave/config.py
@@ -13,6 +13,7 @@ DEFAULT_TTL = int(os.getenv("DEFAULT_TTL", 60))
 SENTRY_DSN = os.getenv("SENTRY_DSN", "")
 VERSION_FILE = os.getenv("VERSION_FILE", "version.json")
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+REQUESTS_MAX_PARALLEL = int(os.getenv("REQUESTS_MAX_PARALLEL", 16))
 LOG_FORMAT = os.getenv("LOG_FORMAT", "json")
 LOGGING = {
     "version": 1,
