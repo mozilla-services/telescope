0ab6c033d041cd87345cc7508d684985b22f5afd
Mathieu Leplatre <mathieu@mozilla.com>
2019-09-23 18:18:56 +0200

    Fix #80: Age work-in-progress (#84)
    * Fix #80: Age work-in-progress
    
    * Show responsible editors

---
+++ checks/remotesettings/work_in_progress.py
@@ -0,0 +1,79 @@
+"""
+Collections should not have very old pending changes.
+
+The list of collections with pending changes is returned, with the age in days
+and the list of responsible editors.
+"""
+import logging
+from datetime import datetime
+
+from poucave.typings import CheckResult
+from poucave.utils import run_parallel, utcnow
+
+from .utils import KintoClient, fetch_signed_resources
+
+
+logger = logging.getLogger(__name__)
+
+
+EXPOSED_PARAMETERS = ["max_age"]
+
+
+async def run(server: str, auth: str, max_age: int) -> CheckResult:
+    resources = await fetch_signed_resources(server, auth)
+
+    client = KintoClient(server_url=server, auth=auth)
+
+    futures = [
+        client.get_collection(
+            bucket=resource["source"]["bucket"], id=resource["source"]["collection"]
+        )
+        for resource in resources
+    ]
+    results = await run_parallel(*futures)
+
+    too_old = {}
+    for resource, resp in zip(resources, results):
+        metadata = resp["data"]
+        # For this check, since we want to detect pending changes,
+        # we also consider work-in-progress a pending request review.
+        if metadata["status"] not in ("work-in-progress", "to-review"):
+            continue
+
+        try:
+            last_edit = metadata["last_edit_date"]
+            dt = datetime.fromisoformat(last_edit)
+            age = (utcnow() - dt).days
+        except KeyError:
+            # Never edited.
+            age = float("inf")
+
+        if age > max_age:
+            # Fetch list of editors, if necessary to contact them.
+            group = await client.get_group(
+                bucket=resource["source"]["bucket"],
+                id=resource["source"]["collection"] + "-editors",
+            )
+            editors = group["data"]["members"]
+
+            cid = "{bucket}/{collection}".format(**resource["destination"])
+            too_old[cid] = {
+                "age": age,
+                "status": metadata["status"],
+                "editors": editors,
+            }
+
+    """
+    {
+      "security-state/cert-revocations": {
+        "age": 82,
+        "status": "to-review",
+        "editors": [
+          "ldap:user1@mozilla.com",
+          "ldap:user2@mozilla.com",
+          "account:crlite_publisher"
+        ]
+      }
+    }
+    """
+    return len(too_old) == 0, too_old
---
+++ tests/checks/remotesettings/test_work_in_progress.py
@@ -0,0 +1,92 @@
+from datetime import timedelta
+
+from poucave.utils import utcnow
+from checks.remotesettings.work_in_progress import run
+
+from tests.utils import patch_async
+
+
+FAKE_AUTH = ""
+COLLECTION_URL = "/buckets/{}/collections/{}"
+GROUP_URL = "/buckets/{}/groups/{}"
+MODULE = "checks.remotesettings.work_in_progress"
+RESOURCES = [
+    {
+        "source": {"bucket": "bid", "collection": "cid"},
+        "destination": {"bucket": "main", "collection": "cid"},
+    },
+    {
+        "source": {"bucket": "bid", "collection": "cid2"},
+        "destination": {"bucket": "main", "collection": "cid2"},
+    },
+]
+
+
+async def test_positive(mock_responses):
+    server_url = "http://fake.local/v1"
+
+    collection_url = server_url + COLLECTION_URL.format("bid", "cid")
+    mock_responses.get(
+        collection_url,
+        payload={
+            "data": {
+                "status": "work-in-progress",
+                "last_edit_date": (utcnow() - timedelta(days=10)).isoformat(),
+            }
+        },
+    )
+    collection_url = server_url + COLLECTION_URL.format("bid", "cid2")
+    mock_responses.get(
+        collection_url,
+        payload={
+            "data": {"status": "signed", "last_edit_date": "2017-08-01T01:00.000"}
+        },
+    )
+
+    with patch_async(f"{MODULE}.fetch_signed_resources", return_value=RESOURCES):
+        status, data = await run(server_url, FAKE_AUTH, max_age=25)
+
+    assert status is True
+    assert data == {}
+
+
+async def test_negative(mock_responses):
+    server_url = "http://fake.local/v1"
+
+    collection_url = server_url + COLLECTION_URL.format("bid", "cid")
+    mock_responses.get(
+        collection_url,
+        payload={
+            "data": {
+                "status": "to-review",
+                "last_edit_date": (utcnow() - timedelta(days=10)).isoformat(),
+            }
+        },
+    )
+    group_url = server_url + GROUP_URL.format("bid", "cid-editors")
+    mock_responses.get(
+        group_url, payload={"data": {"members": ["ldap:user@mozilla.com"]}}
+    )
+    collection_url = server_url + COLLECTION_URL.format("bid", "cid2")
+    mock_responses.get(collection_url, payload={"data": {"status": "work-in-progress"}})
+    group_url = server_url + GROUP_URL.format("bid", "cid2-editors")
+    mock_responses.get(
+        group_url, payload={"data": {"members": ["ldap:editor@mozilla.com"]}}
+    )
+
+    with patch_async(f"{MODULE}.fetch_signed_resources", return_value=RESOURCES):
+        status, data = await run(server_url, FAKE_AUTH, max_age=5)
+
+    assert status is False
+    assert data == {
+        "main/cid": {
+            "age": 10,
+            "status": "to-review",
+            "editors": ["ldap:user@mozilla.com"],
+        },
+        "main/cid2": {
+            "age": float("inf"),
+            "status": "work-in-progress",
+            "editors": ["ldap:editor@mozilla.com"],
+        },
+    }
