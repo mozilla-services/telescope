138cc9a0c8ee6b095f3b17401907179e3eb5c434
Mathieu Leplatre <mathieu@mozilla.com>
2019-10-17 11:45:30 +0200

    Fix #66: fail if some reported recipes do not match (#128)
    * Fix #66: fail if some reported recipes do not match
    
    * Remove leftover

--- checks/normandy/reported_recipes.py
+++ checks/normandy/reported_recipes.py
@@ -6,6 +6,8 @@ The list of recipes for which no event was received is returned. The min/max
 timestamps give the datetime range of the dataset obtained from
 https://sql.telemetry.mozilla.org/queries/64921/
 """
+from collections import defaultdict
+
 import aiohttp
 
 from poucave.typings import CheckResult
@@ -18,14 +20,17 @@ REDASH_QUERY_ID = 64921
 NORMANDY_URL = "{server}/api/v1/recipe/signed/?enabled=1"
 
 
-async def run(api_key: str, server: str) -> CheckResult:
+async def run(api_key: str, server: str, min_total_events: int = 1000) -> CheckResult:
     # Fetch latest results from Redash JSON API.
     rows = await fetch_redash(REDASH_QUERY_ID, api_key)
 
     min_timestamp = min(r["min_timestamp"] for r in rows)
     max_timestamp = max(r["max_timestamp"] for r in rows)
 
-    reported_recipes = set(int(r["source"].split("/")[-1]) for r in rows)
+    count_by_id = defaultdict(int)
+    for row in rows:
+        rid = int(row["source"].split("/")[-1])
+        count_by_id[rid] += row["total"]
 
     async with aiohttp.ClientSession() as session:
         # Recipes from source of truth.
@@ -35,11 +40,17 @@ async def run(api_key: str, server: str) -> CheckResult:
 
     normandy_recipes = set(r["recipe"]["id"] for r in normandy_recipes)
 
+    reported_recipes = set(count_by_id.keys())
+
     missing = list(normandy_recipes - reported_recipes)
+
+    extras = list(reported_recipes - normandy_recipes)
+    extras = [rid for rid in extras if count_by_id[rid] > min_total_events]
 
     data = {
         "min_timestamp": min_timestamp,
         "max_timestamp": max_timestamp,
         "missing": missing,
+        "extras": extras,
     }
-    return len(missing) == 0, data
+    return len(missing) == len(extras) == 0, data
--- tests/checks/normandy/test_reported_recipes.py
+++ tests/checks/normandy/test_reported_recipes.py
@@ -26,6 +26,13 @@ FAKE_ROWS = [
         "min_timestamp": "2019-09-16T01:36:12.348",
         "max_timestamp": "2019-09-16T07:24:58.741",
     },
+    {
+        "status": "custom_2_error",
+        "source": "normandy/recipe/111",
+        "total": 999,
+        "min_timestamp": "2019-09-16T03:36:12.348",
+        "max_timestamp": "2019-09-16T05:24:58.741",
+    },
 ]
 
 
@@ -41,6 +48,7 @@ async def test_positive(mock_aioresponses):
     assert status is True
     assert data == {
         "missing": [],
+        "extras": [],
         "min_timestamp": "2019-09-16T01:36:12.348",
         "max_timestamp": "2019-09-16T07:24:58.741",
     }
@@ -62,6 +70,27 @@ async def test_negative(mock_aioresponses):
     assert status is False
     assert data == {
         "missing": [789],
+        "extras": [],
+        "min_timestamp": "2019-09-16T01:36:12.348",
+        "max_timestamp": "2019-09-16T07:24:58.741",
+    }
+
+
+async def test_negative_min_events(mock_aioresponses):
+    mock_aioresponses.get(
+        NORMANDY_URL.format(server=NORMANDY_SERVER),
+        payload=[{"recipe": {"id": 123}}, {"recipe": {"id": 456}}],
+    )
+
+    with patch_async(f"{MODULE}.fetch_redash", return_value=FAKE_ROWS):
+        status, data = await run(
+            server=NORMANDY_SERVER, api_key="", min_total_events=100
+        )
+
+    assert status is False
+    assert data == {
+        "missing": [],
+        "extras": [111],
         "min_timestamp": "2019-09-16T01:36:12.348",
         "max_timestamp": "2019-09-16T07:24:58.741",
     }
