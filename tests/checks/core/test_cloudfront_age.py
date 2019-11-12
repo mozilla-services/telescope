379e037e522fc19f03dc69fbc81330e45c8dff70
Mathieu Leplatre <mathieu@mozilla.com>
2019-11-12 10:48:11 +0100

    Fix cloudfront age check when age = 0 (#188)

--- tests/checks/core/test_cloudfront_age.py
+++ tests/checks/core/test_cloudfront_age.py
@@ -5,7 +5,9 @@ from checks.core.cloudfront_age import run
 
 async def test_positive(mock_aioresponses):
     url = "http://cdn.server/foo"
-    mock_aioresponses.head(url, status=200, headers={"Age": "23"})
+    mock_aioresponses.head(
+        url, status=200, headers={"Age": "23", "X-Cache": "Hit from cloudfront"}
+    )
 
     status, data = await run(url, max_age=30)
 
@@ -15,12 +17,24 @@ async def test_positive(mock_aioresponses):
 
 async def test_negative(mock_aioresponses):
     url = "http://cdn.server/foo"
-    mock_aioresponses.head(url, status=200, headers={"Age": "33"})
+    mock_aioresponses.head(
+        url, status=200, headers={"Age": "33", "X-Cache": "Hit from cloudfront"}
+    )
 
     status, data = await run(url, max_age=30)
 
     assert status is False
     assert data == 33
+
+
+async def test_positive_fresh_hit(mock_aioresponses):
+    url = "http://cdn.server/foo"
+    mock_aioresponses.head(url, status=200, headers={"X-Cache": "Hit from cloudfront"})
+
+    status, data = await run(url, max_age=30)
+
+    assert status is True
+    assert data == 0
 
 
 async def test_positive_miss(mock_aioresponses):
