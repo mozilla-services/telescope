1b0230c9d4433a9e6eb126aeb6df579454097839
Mathieu Leplatre <mathieu@mozilla.com>
2019-11-19 19:53:12 +0100

    Fix #208: exclude reported recipes that were changed recently (#209)
    * Fix #208: exclude reported recipes that were changed recently
    
    * Adjust style

--- checks/normandy/reported_recipes.py
+++ checks/normandy/reported_recipes.py
@@ -7,11 +7,10 @@ timestamps give the datetime range of the dataset obtained from
 https://sql.telemetry.mozilla.org/queries/64921/
 """
 from collections import defaultdict
-
-import aiohttp
+from datetime import datetime
 
 from poucave.typings import CheckResult
-from poucave.utils import fetch_redash
+from poucave.utils import fetch_json, fetch_redash, run_parallel
 
 
 EXPOSED_PARAMETERS = ["server", "min_total_events"]
@@ -19,6 +18,9 @@ EXPOSED_PARAMETERS = ["server", "min_total_events"]
 REDASH_QUERY_ID = 64921
 
 NORMANDY_URL = "{server}/api/v1/recipe/signed/?enabled=1"
+RECIPE_URL = "{server}/api/v1/recipe/{id}/"
+
+RFC_3339 = "%Y-%m-%dT%H:%M:%S.%fZ"
 
 
 async def run(api_key: str, server: str, min_total_events: int = 1000) -> CheckResult:
@@ -33,25 +35,35 @@ async def run(api_key: str, server: str, min_total_events: int = 1000) -> CheckR
         rid = int(row["source"].split("/")[-1])
         count_by_id[rid] += row["total"]
 
-    async with aiohttp.ClientSession() as session:
-        # Recipes from source of truth.
-        normandy_url = NORMANDY_URL.format(server=server)
-        async with session.get(normandy_url) as response:
-            normandy_recipes = await response.json()
+    # Recipes from source of truth.
+    normandy_url = NORMANDY_URL.format(server=server)
+    normandy_recipes = await fetch_json(normandy_url)
 
-    normandy_recipes = set(r["recipe"]["id"] for r in normandy_recipes)
+    reported_recipes_ids = set(count_by_id.keys())
 
-    reported_recipes = set(count_by_id.keys())
+    normandy_recipes_ids = set(r["recipe"]["id"] for r in normandy_recipes)
+    missing = normandy_recipes_ids - reported_recipes_ids
 
-    missing = list(normandy_recipes - reported_recipes)
+    extras = reported_recipes_ids - normandy_recipes_ids
 
-    extras = list(reported_recipes - normandy_recipes)
-    extras = [rid for rid in extras if count_by_id[rid] > min_total_events]
+    # Exclude recipes for which very few events were received.
+    extras -= set(rid for rid in extras if count_by_id[rid] < min_total_events)
+
+    # Exclude recipes that were modified recently.
+    # (ie. after the Telemetry data window started)
+    min_datetime = datetime.fromisoformat(min_timestamp)
+    futures = [fetch_json(RECIPE_URL.format(server=server, id=rid)) for rid in extras]
+    results = await run_parallel(*futures)
+    extras -= set(
+        rid
+        for rid, details in zip(extras, results)
+        if datetime.strptime(details["last_updated"], RFC_3339) > min_datetime
+    )
 
     data = {
         "min_timestamp": min_timestamp,
         "max_timestamp": max_timestamp,
-        "missing": missing,
-        "extras": extras,
+        "missing": list(missing),
+        "extras": list(extras),
     }
     return len(missing) == len(extras) == 0, data
