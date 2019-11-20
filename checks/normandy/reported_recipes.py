fa5e9eebf1a90d66034e5d53c12bb627fb352e5e
Mathieu Leplatre <mathieu@mozilla.com>
2019-11-20 16:07:53 +0100

    Add lag margin to reported recipes check (#213)
    * Add lag margin to reported recipes check
    
    * Add some debug logging
    
    * Lower default margin to 10min

--- checks/normandy/reported_recipes.py
+++ checks/normandy/reported_recipes.py
@@ -6,14 +6,16 @@ The list of recipes for which no event was received is returned. The min/max
 timestamps give the datetime range of the dataset obtained from
 https://sql.telemetry.mozilla.org/queries/64921/
 """
+import logging
 from collections import defaultdict
-from datetime import datetime
+from datetime import datetime, timedelta
+from typing import Dict
 
 from poucave.typings import CheckResult
 from poucave.utils import fetch_json, fetch_redash, run_parallel
 
 
-EXPOSED_PARAMETERS = ["server", "min_total_events"]
+EXPOSED_PARAMETERS = ["server", "min_total_events", "lag_margin"]
 
 REDASH_QUERY_ID = 64921
 
@@ -23,14 +25,19 @@ RECIPE_URL = "{server}/api/v1/recipe/{id}/"
 RFC_3339 = "%Y-%m-%dT%H:%M:%S.%fZ"
 
 
-async def run(api_key: str, server: str, min_total_events: int = 1000) -> CheckResult:
+logger = logging.getLogger(__name__)
+
+
+async def run(
+    api_key: str, server: str, min_total_events: int = 1000, lag_margin: int = 600
+) -> CheckResult:
     # Fetch latest results from Redash JSON API.
     rows = await fetch_redash(REDASH_QUERY_ID, api_key)
 
     min_timestamp = min(r["min_timestamp"] for r in rows)
     max_timestamp = max(r["max_timestamp"] for r in rows)
 
-    count_by_id = defaultdict(int)
+    count_by_id: Dict[int, int] = defaultdict(int)
     for row in rows:
         rid = int(row["source"].split("/")[-1])
         count_by_id[rid] += row["total"]
@@ -54,10 +61,17 @@ async def run(api_key: str, server: str, min_total_events: int = 1000) -> CheckR
     min_datetime = datetime.fromisoformat(min_timestamp)
     futures = [fetch_json(RECIPE_URL.format(server=server, id=rid)) for rid in extras]
     results = await run_parallel(*futures)
+    for result in results:
+        logger.debug("Extra recipe {id} modified on {last_updated}".format(**result))
+    # We add a lag margin, because modified recipes take some time to reach the
+    # clients. According to current figures obtained from uptake telemetry,
+    # 95% of them obtain the changes in less than ~5min (hence default of 10min).
     extras -= set(
         rid
         for rid, details in zip(extras, results)
-        if datetime.strptime(details["last_updated"], RFC_3339) > min_datetime
+        if datetime.strptime(details["last_updated"], RFC_3339)
+        - timedelta(seconds=lag_margin)
+        > min_datetime
     )
 
     data = {
