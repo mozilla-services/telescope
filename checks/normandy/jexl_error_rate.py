94bcb4d2564248f68610dc06081e7d177afc04c4
Mathieu Leplatre <mathieu@mozilla.com>
2019-12-04 10:45:30 +0100

    Fix #222: Add check for Normandy JEXL error rate (#225)
    * Fix #222: Add check for Normandy classify error rate
    
    * Rename from classify to JEXL

---
+++ checks/normandy/jexl_error_rate.py
@@ -0,0 +1,41 @@
+"""
+The percentage of JEXL filter expressions errors in Normandy should be under the specified
+maximum.
+
+The error rate percentage is returned. The min/max timestamps give the datetime range of the
+dataset obtained from https://sql.telemetry.mozilla.org/queries/64921/
+"""
+from poucave.typings import CheckResult
+from poucave.utils import fetch_redash
+
+
+EXPOSED_PARAMETERS = ["max_error_percentage"]
+
+REDASH_QUERY_ID = 64921
+
+
+async def run(api_key: str, max_error_percentage: float) -> CheckResult:
+    # Fetch latest results from Redash JSON API.
+    rows = await fetch_redash(REDASH_QUERY_ID, api_key)
+
+    min_timestamp = min(r["min_timestamp"] for r in rows)
+    max_timestamp = max(r["max_timestamp"] for r in rows)
+
+    total = sum(row["total"] for row in rows)
+    classify_errors = sum(
+        row["total"] for row in rows if row["status"] == "content_error"
+    )
+    error_rate = classify_errors * 100.0 / total
+    data = {
+        "error_rate": round(error_rate, 2),
+        "min_timestamp": min_timestamp,
+        "max_timestamp": max_timestamp,
+    }
+    """
+    {
+      "error_rate": 2.11,
+      "min_timestamp": "2019-09-19T03:47:42.773",
+      "max_timestamp": "2019-09-19T09:43:26.083"
+    }
+    """
+    return error_rate <= max_error_percentage, data
---
+++ tests/checks/normandy/test_normandy_jexl_error_rate.py
@@ -0,0 +1,53 @@
+from checks.normandy.jexl_error_rate import run
+from tests.utils import patch_async
+
+
+MODULE = "checks.normandy.jexl_error_rate"
+
+FAKE_ROWS = [
+    {
+        "status": "success",
+        "source": "normandy/recipe/123",
+        "total": 1800,
+        "min_timestamp": "2019-09-16T02:36:12.348",
+        "max_timestamp": "2019-09-16T06:24:58.741",
+    },
+    {
+        "status": "content_error",
+        "source": "normandy/recipe/123",
+        "total": 100,
+        "min_timestamp": "2019-09-16T03:36:12.348",
+        "max_timestamp": "2019-09-16T05:24:58.741",
+    },
+    {
+        "status": "content_error",
+        "source": "normandy/recipe/456",
+        "total": 100,
+        "min_timestamp": "2019-09-16T01:36:12.348",
+        "max_timestamp": "2019-09-16T07:24:58.741",
+    },
+]
+
+
+async def test_positive():
+    with patch_async(f"{MODULE}.fetch_redash", return_value=FAKE_ROWS):
+        status, data = await run(api_key="", max_error_percentage=100.0)
+
+    assert status is True
+    assert data == {
+        "error_rate": 10.0,
+        "min_timestamp": "2019-09-16T01:36:12.348",
+        "max_timestamp": "2019-09-16T07:24:58.741",
+    }
+
+
+async def test_negative():
+    with patch_async(f"{MODULE}.fetch_redash", return_value=FAKE_ROWS):
+        status, data = await run(api_key="", max_error_percentage=1.0)
+
+    assert status is False
+    assert data == {
+        "error_rate": 10.0,
+        "min_timestamp": "2019-09-16T01:36:12.348",
+        "max_timestamp": "2019-09-16T07:24:58.741",
+    }
