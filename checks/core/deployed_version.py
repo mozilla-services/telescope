0c539955ae8dc2aa2414bd946ba5903373da13cf
Mathieu Leplatre <mathieu@mozilla.com>
2019-12-10 10:26:14 +0100

    Fix check for deployed version

--- checks/core/deployed_version.py
+++ checks/core/deployed_version.py
@@ -15,10 +15,9 @@ async def run(server: str, repo: str) -> CheckResult:
     version_info = await fetch_json(server + "/__version__")
 
     # Latest Github tag.
-    releases = await fetch_json(f"https://api.github.com/repos/{repo}/releases")
-
+    releases = await fetch_json(f"https://api.github.com/repos/{repo}/releases/latest")
     deployed_version = version_info["version"]
-    latest_tag = releases[0]["tag_name"]
+    latest_tag = releases["tag_name"]
 
     return (
         deployed_version == latest_tag,
--- tests/checks/core/test_deployed_version.py
+++ tests/checks/core/test_deployed_version.py
@@ -15,22 +15,20 @@ async def test_positive(mock_aioresponses):
     )
 
     mock_aioresponses.get(
-        "https://api.github.com/repos/mozilla-services/kinto-dist/releases",
+        "https://api.github.com/repos/mozilla-services/kinto-dist/releases/latest",
         status=200,
-        payload=[
-            {
-                "url": "https://api.github.com/repos/mozilla-services/kinto-dist/releases/18962883",
-                "assets_url": "https://api.github.com/repos/mozilla-services/kinto-dist/releases/18962883/assets",
-                "upload_url": "https://uploads.github.com/repos/mozilla-services/kinto-dist/releases/18962883/assets{?name,label}",
-                "html_url": "https://github.com/mozilla-services/kinto-dist/releases/tag/17.1.4",
-                "id": 18962883,
-                "node_id": "MDc6UmVsZWFzZTE4OTYyODgz",
-                "tag_name": "17.1.4",
-                "target_commitish": "master",
-                "name": "",
-                "author": {"login": "leplatrem", "id": 546692},
-            }
-        ],
+        payload={
+            "url": "https://api.github.com/repos/mozilla-services/kinto-dist/releases/18962883",
+            "assets_url": "https://api.github.com/repos/mozilla-services/kinto-dist/releases/18962883/assets",
+            "upload_url": "https://uploads.github.com/repos/mozilla-services/kinto-dist/releases/18962883/assets{?name,label}",
+            "html_url": "https://github.com/mozilla-services/kinto-dist/releases/tag/17.1.4",
+            "id": 18962883,
+            "node_id": "MDc6UmVsZWFzZTE4OTYyODgz",
+            "tag_name": "17.1.4",
+            "target_commitish": "master",
+            "name": "",
+            "author": {"login": "leplatrem", "id": 546692},
+        },
     )
 
     status, data = await run(server=url, repo="mozilla-services/kinto-dist")
@@ -53,22 +51,20 @@ async def test_negative(mock_aioresponses):
     )
 
     mock_aioresponses.get(
-        "https://api.github.com/repos/mozilla-services/kinto-dist/releases",
+        "https://api.github.com/repos/mozilla-services/kinto-dist/releases/latest",
         status=200,
-        payload=[
-            {
-                "url": "https://api.github.com/repos/mozilla-services/kinto-dist/releases/19949437",
-                "assets_url": "https://api.github.com/repos/mozilla-services/kinto-dist/releases/19949437/assets",
-                "upload_url": "https://uploads.github.com/repos/mozilla-services/kinto-dist/releases/19949437/assets{?name,label}",
-                "html_url": "https://github.com/mozilla-services/kinto-dist/releases/tag/17.2.0",
-                "id": 19949437,
-                "node_id": "MDc6UmVsZWFzZTE5OTQ5NDM3",
-                "tag_name": "17.2.0",
-                "target_commitish": "master",
-                "name": "",
-                "author": {"login": "leplatrem", "id": 546692},
-            }
-        ],
+        payload={
+            "url": "https://api.github.com/repos/mozilla-services/kinto-dist/releases/19949437",
+            "assets_url": "https://api.github.com/repos/mozilla-services/kinto-dist/releases/19949437/assets",
+            "upload_url": "https://uploads.github.com/repos/mozilla-services/kinto-dist/releases/19949437/assets{?name,label}",
+            "html_url": "https://github.com/mozilla-services/kinto-dist/releases/tag/17.2.0",
+            "id": 19949437,
+            "node_id": "MDc6UmVsZWFzZTE5OTQ5NDM3",
+            "tag_name": "17.2.0",
+            "target_commitish": "master",
+            "name": "",
+            "author": {"login": "leplatrem", "id": 546692},
+        },
     )
 
     status, data = await run(server=url, repo="mozilla-services/kinto-dist")
