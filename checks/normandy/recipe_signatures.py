4caf14d13b9e1e8d96e9becd2f077da9e6ff1e62
Mathieu Leplatre <mathieu@mozilla.com>
2019-12-11 12:13:59 +0100

    Fix #240: add check to verify Normandy recipes signatures (#243)
    * Fix #240: add check to verify Normandy recipes signatures
    
    * Add integration test for x5us
    
    * Rename modules
    
    * Add docstring

---
+++ checks/normandy/recipe_signatures.py
@@ -0,0 +1,54 @@
+"""
+Signatures should be valid for each published recipe.
+
+The list of failing recipes is returned.
+"""
+import json
+import logging
+
+from autograph_utils import MemoryCache, SignatureVerifier, decode_mozilla_hash
+
+from poucave.typings import CheckResult
+from poucave.utils import ClientSession, fetch_json
+
+
+RECIPES_URL = "{server}/buckets/main/collections/{collection}/records"
+
+
+logger = logging.getLogger(__name__)
+
+
+async def validate_signature(verifier, recipe):
+    signature_payload = recipe["signature"]
+    x5u = signature_payload["x5u"]
+    signature = signature_payload["signature"]
+    attributes = recipe["recipe"]
+    data = json.dumps(attributes, sort_keys=True, separators=(",", ":")).encode("utf8")
+    return await verifier.verify(data, signature, x5u)
+
+
+async def run(server: str, collection: str, root_hash: str) -> CheckResult:
+    """Fetch recipes from Remote Settings and verify that each attached signature
+    is verified with the related recipe attributes.
+
+    :param server: URL of Remote Settings server.
+    :param collection: Collection id to obtain recipes from (eg. ``"normandy-recipes"``.
+    :param root_hash: The expected hash for the first certificate in a chain.
+    """
+    resp = await fetch_json(RECIPES_URL.format(server=server, collection=collection))
+    recipes = resp["data"]
+
+    cache = MemoryCache()
+
+    errors = {}
+
+    async with ClientSession() as session:
+        verifier = SignatureVerifier(session, cache, decode_mozilla_hash(root_hash))
+
+        for recipe in recipes:
+            try:
+                await validate_signature(verifier, recipe)
+            except Exception as e:
+                errors[recipe["id"]] = repr(e)
+
+    return len(errors) == 0, errors
---
+++ tests/checks/normandy/test_recipe_signatures.py
@@ -0,0 +1,112 @@
+from unittest import mock
+
+import autograph_utils
+import pytest
+
+from checks.normandy.recipe_signatures import run, validate_signature
+from poucave.utils import ClientSession
+from tests.utils import patch_async
+
+
+MODULE = "checks.normandy.recipe_signatures"
+RECORDS_URL = "/buckets/{}/collections/{}/records"
+
+CERT = """-----BEGIN CERTIFICATE-----
+MIIDBTCCAougAwIBAgIIFcbkDrCrHAkwCgYIKoZIzj0EAwMwgaMxCzAJBgNVBAYT
+AlVTMRwwGgYDVQQKExNNb3ppbGxhIENvcnBvcmF0aW9uMS8wLQYDVQQLEyZNb3pp
+bGxhIEFNTyBQcm9kdWN0aW9uIFNpZ25pbmcgU2VydmljZTFFMEMGA1UEAww8Q29u
+dGVudCBTaWduaW5nIEludGVybWVkaWF0ZS9lbWFpbEFkZHJlc3M9Zm94c2VjQG1v
+emlsbGEuY29tMB4XDTE5MDgyMzIyNDQzMVoXDTE5MTExMTIyNDQzMVowgakxCzAJ
+BgNVBAYTAlVTMRMwEQYDVQQIEwpDYWxpZm9ybmlhMRYwFAYDVQQHEw1Nb3VudGFp
+biBWaWV3MRwwGgYDVQQKExNNb3ppbGxhIENvcnBvcmF0aW9uMRcwFQYDVQQLEw5D
+bG91ZCBTZXJ2aWNlczE2MDQGA1UEAxMtcGlubmluZy1wcmVsb2FkLmNvbnRlbnQt
+c2lnbmF0dXJlLm1vemlsbGEub3JnMHYwEAYHKoZIzj0CAQYFK4EEACIDYgAEX6Zd
+vZ32rj9rDdRInp0kckbMtAdxOQxJ7EVAEZB2KOLUyotQL6A/9YWrMB4Msb4hfvxj
+Nw05CS5/J4qUmsTkKLXQskjRe9x96uOXxprWiVwR4OLYagkJJR7YG1mTXmFzo4GD
+MIGAMA4GA1UdDwEB/wQEAwIHgDATBgNVHSUEDDAKBggrBgEFBQcDAzAfBgNVHSME
+GDAWgBSgHUoXT4zCKzVF8WPx2nBwp8744TA4BgNVHREEMTAvgi1waW5uaW5nLXBy
+ZWxvYWQuY29udGVudC1zaWduYXR1cmUubW96aWxsYS5vcmcwCgYIKoZIzj0EAwMD
+aAAwZQIxAOi2Eusi6MtEPOARiU+kZIi1vPnzTI71cA2ZIpzZ9aYg740eoJml8Guz
+3oC6yXiIDAIwSy4Eylf+/nSMA73DUclcCjZc2yfRYIogII+krXBxoLkbPJcGaitx
+qvRy6gQ1oC/z
+-----END CERTIFICATE-----
+"""
+
+
+async def test_positive(mock_aioresponses):
+    server_url = "http://fake.local/v1"
+    records_url = server_url + RECORDS_URL.format("main", "normandy-recipes")
+    mock_aioresponses.get(
+        records_url,
+        payload={
+            "data": [
+                {
+                    "id": "12",
+                    "last_modified": 42,
+                    "signature": {"signature": "abc", "x5u": "http://fake-x5u-url"},
+                    "recipe": {"id": 12},
+                }
+            ]
+        },
+    )
+
+    with patch_async(f"{MODULE}.validate_signature", return_value=True):
+        status, data = await run(server_url, "normandy-recipes", root_hash="AA")
+
+    assert status is True
+    assert data == {}
+
+
+async def test_negative(mock_aioresponses):
+    server_url = "http://fake.local/v1"
+    records_url = server_url + RECORDS_URL.format("main", "normandy-recipes")
+    mock_aioresponses.get(
+        records_url,
+        payload={
+            "data": [
+                {
+                    "id": "12",
+                    "last_modified": 42,
+                    "signature": {"signature": "abc", "x5u": "http://fake-x5u-url"},
+                    "recipe": {"id": 12},
+                }
+            ]
+        },
+    )
+
+    with patch_async(f"{MODULE}.validate_signature", side_effect=ValueError("boom")):
+        status, data = await run(server_url, "normandy-recipes", root_hash="AA")
+
+    assert status is False
+    assert data == {"12": "ValueError('boom')"}
+
+
+async def test_invalid_x5u(mock_aioresponses):
+    x5u = "http://fake-x5u-url"
+    mock_aioresponses.get(x5u, body=CERT)
+    cache = autograph_utils.MemoryCache()
+    async with ClientSession() as session:
+        verifier = autograph_utils.SignatureVerifier(session, cache, "fake-hash")
+
+        recipe = {
+            "signature": {"signature": "abc", "x5u": x5u},
+            "recipe": {"id": 12},
+        }
+
+        with pytest.raises(autograph_utils.BadCertificate):
+            await validate_signature(verifier, recipe)
+
+
+async def test_invalid_signature():
+    verifier = mock.MagicMock()
+    verifier.verify.side_effect = autograph_utils.BadSignature
+
+    recipe = {
+        "signature": {"signature": "abc", "x5u": "http://fake-x5u-url"},
+        "recipe": {"id": 12},
+    }
+
+    with pytest.raises(autograph_utils.BadSignature):
+        await validate_signature(verifier, recipe)
+
+    verifier.verify.assert_called_with(b'{"id":12}', "abc", "http://fake-x5u-url")
