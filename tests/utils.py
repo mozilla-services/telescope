900ef57d2d6638cb3fab566c2917f5b888c7504c
Ethan Glasser-Camp <ethan@betacantrips.com>
2019-12-30 17:05:23 -0500

    Fix tests on Python 3.8.1

--- tests/utils.py
+++ tests/utils.py
@@ -1,19 +1,26 @@
 import asyncio
+import sys
 from contextlib import contextmanager
 from unittest import mock
 
 
-@contextmanager
-def patch_async(*args, return_value=None, **kwargs):
-    side_effect = kwargs.pop("side_effect", None)
+if sys.version_info >= (3, 8, 1):
+    # mock.patch automatically detects async stuff and returns an
+    # AsyncMock as of 3.8.1
+    patch_async = mock.patch
+else:
+    # Otherwise, work around it ourselves
+    @contextmanager
+    def patch_async(*args, return_value=None, **kwargs):
+        side_effect = kwargs.pop("side_effect", None)
 
-    with mock.patch(*args, **kwargs) as m:
-        f = asyncio.Future()
+        with mock.patch(*args, **kwargs) as m:
+            f = asyncio.Future()
 
-        if side_effect is not None:
-            f.set_exception(side_effect)
-        else:
-            f.set_result(return_value)
+            if side_effect is not None:
+                f.set_exception(side_effect)
+            else:
+                f.set_result(return_value)
 
-        m.return_value = f
-        yield m
+            m.return_value = f
+            yield m
