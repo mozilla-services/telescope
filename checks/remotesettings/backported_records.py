b26bbb1dd719004adaa643c18ef7e8a5577fedb4
Mathieu Leplatre <mathieu@mozilla.com>
2020-01-10 12:11:25 +0100

    Compare everything in backport records check

--- checks/remotesettings/backported_records.py
+++ checks/remotesettings/backported_records.py
@@ -1,15 +1,15 @@
 """
-The collection timestamp of the destination, where the source records are backported,
-shouldn't lag by more than some number of seconds. The default max lag is 5 minutes, which
-is the run frequency of the backport lambda.
+The backported collection should contain exactly the same records as the source,
+with a maximum lag in seconds. The default max lag is 300, which is the run frequency
+of the backport lambda (5 min).
 
-The source and destination collection timestamps are returned.
+The differences between source and destination are returned.
 """
 from typing import Dict
 
 from poucave.typings import CheckResult
 
-from .utils import KintoClient
+from .utils import KintoClient, compare_collections, human_diff
 
 
 EXPOSED_PARAMETERS = ["server", "max_lag_seconds"]
@@ -25,20 +25,22 @@ async def run(
         source_bid, source_cid = source.split("/")
         dest_bid, dest_cid = dest.split("/")
 
-        source_timestamp = await client.get_records_timestamp(
+        source_records = await client.get_records(
             bucket=source_bid, collection=source_cid
         )
-        dest_timestamp = await client.get_records_timestamp(
-            bucket=dest_bid, collection=dest_cid
-        )
-
-        diff_millisecond = abs(int(source_timestamp) - int(dest_timestamp))
-        if (diff_millisecond / 1000) > max_lag_seconds:
-            errors.append(
-                {
-                    f"{source_bid}/{source_cid}": source_timestamp,
-                    f"{dest_bid}/{dest_cid}": dest_timestamp,
-                }
+        dest_records = await client.get_records(bucket=dest_bid, collection=dest_cid)
+        diff = compare_collections(source_records, dest_records)
+        if diff:
+            source_timestamp = await client.get_records_timestamp(
+                bucket=source_bid, collection=source_cid
             )
+            dest_timestamp = await client.get_records_timestamp(
+                bucket=dest_bid, collection=dest_cid
+            )
+            diff_millisecond = abs(int(source_timestamp) - int(dest_timestamp))
+            if (diff_millisecond / 1000) > max_lag_seconds:
+                missing, differ, extras = diff
+                details = human_diff(source, dest, missing, differ, extras)
+                errors.append(details)
 
     return len(errors) == 0, errors
--- tests/checks/remotesettings/test_backported_records.py
+++ tests/checks/remotesettings/test_backported_records.py
@@ -7,9 +7,34 @@ RECORDS_URL = "/buckets/{}/collections/{}/records"
 async def test_positive(mock_responses):
     server_url = "http://fake.local/v1"
     source_url = server_url + RECORDS_URL.format("bid", "cid")
-    mock_responses.head(source_url, headers={"ETag": '"12345"'})
+    mock_responses.get(
+        source_url, payload={"data": [{"id": "abc", "last_modified": 42}]}
+    )
     dest_url = server_url + RECORDS_URL.format("other", "cid")
-    mock_responses.head(dest_url, headers={"ETag": '"12345"'})
+    mock_responses.get(dest_url, payload={"data": [{"id": "abc", "last_modified": 43}]})
+
+    status, data = await run(
+        server_url, backports={"bid/cid": "other/cid"}, max_lag_seconds=1
+    )
+
+    assert status is True
+    assert data == []
+
+
+async def test_positive_small_lag(mock_responses):
+    server_url = "http://fake.local/v1"
+    source_url = server_url + RECORDS_URL.format("bid", "cid")
+    mock_responses.get(
+        source_url,
+        payload={"data": [{"id": "abc", "last_modified": 42}]},
+        headers={"ETag": '"100"'},
+    )
+    dest_url = server_url + RECORDS_URL.format("other", "cid")
+    mock_responses.get(
+        dest_url,
+        payload={"data": [{"id": "abc", "last_modified": 43, "title": "abc"}]},
+        headers={"ETag": '"150"'},
+    )
 
     status, data = await run(
         server_url, backports={"bid/cid": "other/cid"}, max_lag_seconds=1
@@ -22,13 +47,21 @@ async def test_positive(mock_responses):
 async def test_negative(mock_responses):
     server_url = "http://fake.local/v1"
     source_url = server_url + RECORDS_URL.format("bid", "cid")
-    mock_responses.head(source_url, headers={"ETag": '"1000000"'})
+    mock_responses.get(
+        source_url,
+        payload={"data": [{"id": "abc", "last_modified": 42}]},
+        headers={"ETag": '"1000000"'},
+    )
     dest_url = server_url + RECORDS_URL.format("other", "cid")
-    mock_responses.head(dest_url, headers={"ETag": '"2000000"'})
+    mock_responses.get(
+        dest_url,
+        payload={"data": [{"id": "abc", "last_modified": 43, "title": "abc"}]},
+        headers={"ETag": '"2000000"'},
+    )
 
     status, data = await run(
         server_url, backports={"bid/cid": "other/cid"}, max_lag_seconds=1
     )
 
     assert status is False
-    assert data == [{"bid/cid": "1000000", "other/cid": "2000000"}]
+    assert data == ["1 record differ between bid/cid and other/cid ('abc')"]
