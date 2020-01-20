91d9a1ffee6849f2700d0abe51d06e4c1982fd19
Mathieu Leplatre <mathieu@mozilla.com>
2020-01-20 15:47:07 +0100

    Fix #227: compute error rate over periods of 10 minutes (#307)
    * Fix #227: compute error by period
    
    * Check error rates by period for Normandy uptake

--- checks/remotesettings/uptake_error_rate.py
+++ checks/remotesettings/uptake_error_rate.py
@@ -1,13 +1,13 @@
 """
 The percentage of reported errors in Uptake Telemetry should be under the specified
-maximum.
+maximum. Error rate is computed for each period of 10min.
 
 For each source whose error rate is above the maximum, the total number of events
 for each status is returned. The min/max timestamps give the datetime range of the
-dataset obtained from https://sql.telemetry.mozilla.org/queries/64808/
+dataset obtained from https://sql.telemetry.mozilla.org/queries/67605/
 """
 from collections import defaultdict
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 from poucave.typings import CheckResult
 from poucave.utils import fetch_redash
@@ -20,7 +20,7 @@ EXPOSED_PARAMETERS = [
     "ignore_versions",
 ]
 
-REDASH_QUERY_ID = 65039
+REDASH_QUERY_ID = 67605
 
 
 def sort_dict_desc(d, key):
@@ -30,7 +30,7 @@ def sort_dict_desc(d, key):
 async def run(
     api_key: str,
     max_error_percentage: float,
-    min_total_events: int = 10000,
+    min_total_events: int = 1000,
     sources: List[str] = [],
     ignore_status: List[str] = [],
     ignore_versions: List[int] = [],
@@ -41,47 +41,78 @@ async def run(
     min_timestamp = min(r["min_timestamp"] for r in rows)
     max_timestamp = max(r["max_timestamp"] for r in rows)
 
-    by_collection: Dict[str, Dict[str, Dict[str, int]]] = defaultdict(
-        lambda: defaultdict(dict)
-    )
+    # We will store reported events by period, by collection,
+    # by version, and by status.
+    # {
+    #   ('2020-01-17T07:50:00', '2020-01-17T08:00:00'): {
+    #     'settings-sync': {
+    #       '71': {
+    #         'success': 4699,
+    #         'sync_error': 39
+    #       },
+    #       ...
+    #     },
+    #     ...
+    #   }
+    # }
+    periods: Dict[Tuple[str, str], Dict] = {}
     for row in rows:
+        period: Tuple[str, str] = (row["min_timestamp"], row["max_timestamp"])
+        if period not in periods:
+            by_collection: Dict[str, Dict[str, Dict[str, int]]] = defaultdict(
+                lambda: defaultdict(dict)
+            )
+            periods[period] = by_collection
+
         if len(sources) == 0 or row["source"] in sources:
-            by_collection[row["source"]][row["version"]][row["status"]] = row["total"]
+            periods[period][row["source"]][row["version"]][row["status"]] = row["total"]
 
-    error_rates = {}
-    for cid, all_versions in by_collection.items():
-        total_statuses = 0
-        statuses: Dict[str, int] = defaultdict(int)
-        ignored: Dict[str, int] = defaultdict(int)
+    error_rates: Dict[str, Dict] = {}
+    for (min_period, max_period), by_collection in periods.items():
+        # Compute error rate by period.
+        # This allows us to prevent error rate to be "spread" over the overall datetime
+        # range of events (eg. a spike of errors during 10min over 2H).
+        for cid, all_versions in by_collection.items():
+            total_statuses = 0
+            # Store total by status (which are not ignored).
+            statuses: Dict[str, int] = defaultdict(int)
+            ignored: Dict[str, int] = defaultdict(int)
 
-        for version, all_statuses in all_versions.items():
-            for status, total in all_statuses.items():
-                total_statuses += total
-                if status in ignore_status or int(version) in ignore_versions:
-                    ignored[status] += total
-                else:
-                    statuses[status] += total
+            for version, all_statuses in all_versions.items():
+                for status, total in all_statuses.items():
+                    total_statuses += total
+                    if status in ignore_status or int(version) in ignore_versions:
+                        ignored[status] += total
+                    else:
+                        statuses[status] += total
 
-        # Ignore uptake Telemetry of a certain collection if the total of collected
-        # events is too small.
-        if total_statuses < min_total_events:
-            continue
+            # Ignore uptake Telemetry of a certain collection if the total of collected
+            # events is too small.
+            if total_statuses < min_total_events:
+                continue
 
-        total_errors = sum(
-            total for status, total in statuses.items() if status.endswith("_error")
+            total_errors = sum(
+                total for status, total in statuses.items() if status.endswith("_error")
+            )
+            error_rate = round(total_errors * 100 / total_statuses, 2)
+
+            # If error rate for this period is below threshold, or lower than one reported
+            # in another period, then we ignore it.
+            other_period_rate = error_rates.get(cid, {"error_rate": 0.0})["error_rate"]
+            if error_rate < max_error_percentage or error_rate < other_period_rate:
+                continue
+
+            error_rates[cid] = {
+                "error_rate": error_rate,
+                "statuses": sort_dict_desc(statuses, key=lambda item: item[1]),
+                "ignored": sort_dict_desc(ignored, key=lambda item: item[1]),
+                "min_timestamp": min_period,
+                "max_timestamp": max_period,
+            }
+
+        sort_by_rate = sort_dict_desc(
+            error_rates, key=lambda item: item[1]["error_rate"]
         )
-        error_rate = round(total_errors * 100 / total_statuses, 2)
-
-        if error_rate < max_error_percentage:
-            continue
-
-        error_rates[cid] = {
-            "error_rate": error_rate,
-            "statuses": sort_dict_desc(statuses, key=lambda item: item[1]),
-            "ignored": sort_dict_desc(ignored, key=lambda item: item[1]),
-        }
-
-    sort_by_rate = sort_dict_desc(error_rates, key=lambda item: item[1]["error_rate"])
 
     data = {
         "sources": sort_by_rate,
@@ -103,11 +134,14 @@ async def run(
           },
           "ignored": {
             "network_error": 10476
-          }
-        }
+          },
+          "min_timestamp": "2020-01-17T08:10:00",
+          "max_timestamp": "2020-01-17T08:20:00",
+        },
+        ...
       },
-      "min_timestamp": "2019-09-16T03:40:57.894",
-      "max_timestamp": "2019-09-16T09:34:07.163"
+      "min_timestamp": "2020-01-17T08:00:00",
+      "max_timestamp": "2020-01-17T10:00:00"
     }
     """
     return len(sort_by_rate) == 0, data
