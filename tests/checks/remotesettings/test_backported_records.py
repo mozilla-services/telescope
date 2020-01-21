ed55ed9a9ce60a909744aa0bfd779ee16b0ac7c9
Mathieu Leplatre <mathieu@mozilla.com>
2020-01-21 15:31:47 +0100

    Support filters in backported records check (#313)

--- tests/checks/remotesettings/test_backported_records.py
+++ tests/checks/remotesettings/test_backported_records.py
@@ -65,3 +65,20 @@ async def test_negative(mock_responses):
 
     assert status is False
     assert data == ["1 record differ between bid/cid and other/cid ('abc')"]
+
+
+async def test_with_filters(mock_responses):
+    server_url = "http://fake.local/v1"
+    source_url = server_url + RECORDS_URL.format("bid", "cid") + "?field.test=42"
+    mock_responses.get(
+        source_url, payload={"data": [{"id": "abc", "last_modified": 42}]}
+    )
+    dest_url = server_url + RECORDS_URL.format("other", "cid")
+    mock_responses.get(dest_url, payload={"data": [{"id": "abc", "last_modified": 43}]})
+
+    status, data = await run(
+        server_url, backports={"bid/cid?field.test=42": "other/cid"}, max_lag_seconds=1
+    )
+
+    assert status is True
+    assert data == []
