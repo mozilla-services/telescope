5cdbaf1c24ad4109ef60a855d4f19addaa896370
Mathieu Leplatre <mathieu@mozilla.com>
2020-01-21 12:25:59 +0100

    Support filters in backported records check

--- checks/remotesettings/backported_records.py
+++ checks/remotesettings/backported_records.py
@@ -5,7 +5,8 @@ of the backport lambda (5 min).
 
 The differences between source and destination are returned.
 """
-from typing import Dict
+from typing import Any, Dict
+from urllib.parse import parse_qs
 
 from poucave.typings import CheckResult
 
@@ -22,11 +23,18 @@ async def run(
 
     errors = []
     for source, dest in backports.items():
+        # If source is filtered, then the check should take it into account.
+        filters: Dict[str, Any] = {}
+        if "?" in source:
+            source, qs = source.split("?")
+            filters = parse_qs(qs)
+            filters = {k: v[0] if len(v) == 1 else v for k, v in filters.items()}
+
         source_bid, source_cid = source.split("/")
         dest_bid, dest_cid = dest.split("/")
 
         source_records = await client.get_records(
-            bucket=source_bid, collection=source_cid
+            bucket=source_bid, collection=source_cid, **filters
         )
         dest_records = await client.get_records(bucket=dest_bid, collection=dest_cid)
         diff = compare_collections(source_records, dest_records)
