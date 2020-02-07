c5a3487038861af5ef92db4deda184c4c51acdfd
Mathieu Leplatre <mathieu@mozilla.com>
2020-02-07 09:35:10 +0100

    Add status parameter to heartbeat check

--- checks/core/heartbeat.py
+++ checks/core/heartbeat.py
@@ -9,15 +9,15 @@ from poucave.typings import CheckResult
 from poucave.utils import ClientSession, retry_decorator
 
 
-EXPOSED_PARAMETERS = ["url"]
+EXPOSED_PARAMETERS = ["url", "status"]
 
 
 @retry_decorator
-async def run(url: str) -> CheckResult:
+async def run(url: str, status: int = 200) -> CheckResult:
     async with ClientSession() as session:
         try:
             async with session.get(url) as response:
-                status = response.status == 200
+                status = response.status == status
                 return status, await response.json()
         except aiohttp.client_exceptions.ClientError as e:
             return False, str(e)
