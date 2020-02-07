66196c5d91171020176cd4e4f8df9d91c78eeff4
Mathieu Leplatre <mathieu@mozilla.com>
2020-02-07 11:16:22 +0100

    Rename parameter as suggested by @smarnach

--- checks/core/heartbeat.py
+++ checks/core/heartbeat.py
@@ -9,15 +9,15 @@ from poucave.typings import CheckResult
 from poucave.utils import ClientSession, retry_decorator
 
 
-EXPOSED_PARAMETERS = ["url", "status"]
+EXPOSED_PARAMETERS = ["url", "expected_status"]
 
 
 @retry_decorator
-async def run(url: str, status: int = 200) -> CheckResult:
+async def run(url: str, expected_status: int = 200) -> CheckResult:
     async with ClientSession() as session:
         try:
             async with session.get(url) as response:
-                status = response.status == status
-                return status, await response.json()
+                success = response.status == expected_status
+                return success, await response.json()
         except aiohttp.client_exceptions.ClientError as e:
             return False, str(e)
