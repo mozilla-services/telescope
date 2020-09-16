9326ea4183129409e307c00c07d9f1be712decb6
Mathieu Leplatre <mathieu@mozilla.com>
2020-09-16 12:14:07 +0200

    Rename master to main

--- scripts/deploy-dockerhub.sh
+++ scripts/deploy-dockerhub.sh
@@ -7,7 +7,7 @@ echo "$DOCKER_PASS" | docker login -u="$DOCKER_USER" --password-stdin
 
 # docker tag and push git branch to dockerhub
 if [ -n "$1" ]; then
-    [ "$1" == master ] && TAG=latest || TAG="$1"
+    [ "$1" == main ] && TAG=latest || TAG="$1"
     docker tag app:build "$DOCKERHUB_REPO:$TAG" ||
         (echo "Couldn't tag app:build as $DOCKERHUB_REPO:$TAG" && false)
     docker push "$DOCKERHUB_REPO:$TAG" ||
