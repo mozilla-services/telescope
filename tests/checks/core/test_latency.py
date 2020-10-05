d3ccc12bdf743aa8396c7d760f943d90bdb27e0b
Mathieu Leplatre <mathieu@mozilla.com>
2020-10-05 15:51:05 +0200

    Address @smarnach feedback

--- tests/checks/core/test_latency.py
+++ tests/checks/core/test_latency.py
@@ -7,30 +7,32 @@ from checks.core.latency import run
 
 async def test_positive(mock_aioresponses):
     def callback(url, **kwargs):
-        time.sleep(0.01)
+        time.sleep(0.100)
         return CallbackResult(status=200)
 
     url = "http://server.local/__heartbeat__"
     mock_aioresponses.get(url, callback=callback)
 
-    status, data = await run(url, max_milliseconds=20)
+    status, data = await run(url, max_milliseconds=200)
 
     assert status is True
-    assert 10 <= data < 20
+    assert 100 <= data < 200
 
 
 async def test_negative(mock_aioresponses):
     def callback(url, **kwargs):
-        time.sleep(0.01)
+        time.sleep(0.100)
         return CallbackResult(status=200)
 
     url = "http://server.local/__heartbeat__"
     mock_aioresponses.get(url, callback=callback)
 
-    status, data = await run(url, max_milliseconds=9)
+    # Since Python 3.5, time.sleep() is guaranteed to
+    # sleep at least for the time given.
+    status, data = await run(url, max_milliseconds=100)
 
     assert status is False
-    assert data >= 10
+    assert data >= 100
 
 
 async def test_unreachable(mock_aioresponses):
