40521b3c4214587ad948658e17d60129502dee4a
Mathieu Leplatre <mathieu@mozilla.com>
2021-02-04 19:00:17 +0100

    Fix #767: check expiration of whole cert chain (#768)
    * Fix #767: check expiration of whole cert chain
    
    * Use cert lifespan to compute expiration alert (thanks @mythmon for the idea)
    
    * Add missing type annotations

--- checks/remotesettings/certificates_expiration.py
+++ checks/remotesettings/certificates_expiration.py
@@ -6,9 +6,11 @@ expiration date and x5u URL.
 """
 import datetime
 import logging
+from typing import Dict, Tuple
 
 import cryptography
 import cryptography.x509
+from autograph_utils import split_pem
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
 
 from poucave.typings import CheckResult
@@ -22,13 +24,22 @@ logger = logging.getLogger(__name__)
 
 EXPOSED_PARAMETERS = ["server", "min_remaining_days"]
 
+# Bound the alert thresholds.
+LOWER_MIN_REMAINING_DAYS = 7
+UPPER_MIN_REMAINING_DAYS = 60
 
-async def fetch_cert(x5u):
+
+async def fetch_certs(x5u):
     cert_pem = await fetch_text(x5u)
-    cert = cryptography.x509.load_pem_x509_certificate(
-        cert_pem.encode("utf-8"), crypto_default_backend()
-    )
-    return cert
+    logger.debug(f"Parse PEM file from {x5u}")
+    pems = split_pem(cert_pem.encode("utf-8"))
+    certs = [
+        cryptography.x509.load_pem_x509_certificate(
+            pem, backend=crypto_default_backend()
+        )
+        for pem in pems
+    ]
+    return certs
 
 
 async def fetch_collection_metadata(server_url, entry):
@@ -39,7 +50,11 @@ async def fetch_collection_metadata(server_url, entry):
     return collection["data"]
 
 
-async def run(server: str, min_remaining_days: int) -> CheckResult:
+async def run(
+    server: str,
+    percentage_remaining_validity: int = 10,
+    min_remaining_days: int = LOWER_MIN_REMAINING_DAYS,
+) -> CheckResult:
     client = KintoClient(server_url=server)
     entries = await client.get_monitor_changes()
 
@@ -50,23 +65,38 @@ async def run(server: str, min_remaining_days: int) -> CheckResult:
 
     # Second, deduplicate the list of x5u URLs and fetch them in parallel.
     x5us = list(set(metadata["signature"]["x5u"] for metadata in results))
-    futures = [fetch_cert(x5u) for x5u in x5us]
+    futures = [fetch_certs(x5u) for x5u in x5us]
     results = await run_parallel(*futures)
-    expirations = {
-        x5u: cert.not_valid_after.replace(tzinfo=datetime.timezone.utc)
-        for x5u, cert in zip(x5us, results)
-    }
+
+    validity: Dict[str, Tuple] = {}
+    for x5u, certs in zip(x5us, results):
+        # For each cert of the chain, keep track of the one that ends the earliest.
+        for cert in certs:
+            end = cert.not_valid_after.replace(tzinfo=datetime.timezone.utc)
+            if x5u not in validity or end < validity[x5u][0]:
+                start = cert.not_valid_before.replace(tzinfo=datetime.timezone.utc)
+                lifespan = (end - start).days
+                validity[x5u] = end, lifespan
 
     # Return collections whose certificate expires too soon.
-    errors = {}
+    errors: Dict[str, Dict] = {}
     for entry, metadata in entries_metadata:
         cid = "{bucket}/{collection}".format(**entry)
         x5u = metadata["signature"]["x5u"]
+        end, lifespan = validity[x5u]
 
-        expiration = expirations[x5u]
-        remaining_days = (expiration - utcnow()).days
-        if remaining_days < min_remaining_days:
-            errors[cid] = {"x5u": x5u, "expires": expiration.isoformat()}
+        # The minimum remaining days depends on the certificate lifespan.
+        relative_minimum = lifespan * percentage_remaining_validity / 100
+        bounded_minimum = int(
+            min(UPPER_MIN_REMAINING_DAYS, max(min_remaining_days, relative_minimum))
+        )
+        remaining_days = (end - utcnow()).days
+        logger.debug(
+            f"{cid} cert lasts {lifespan} days and ends in {remaining_days} days "
+            f"({remaining_days - bounded_minimum} days before alert)."
+        )
+        if remaining_days < bounded_minimum:
+            errors[cid] = {"x5u": x5u, "expires": end.isoformat()}
 
     """
     {
