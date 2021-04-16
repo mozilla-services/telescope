c930538a348a832ae7c69d29c52908eb73760f34
Mathieu Leplatre <mathieu@mozilla.com>
2021-04-16 11:42:47 +0200

    Add max_days_last_activity parameter

--- checks/core/maintenance.py
+++ checks/core/maintenance.py
@@ -1,7 +1,8 @@
 """
 Inspect the state of maintenance of a list of Github repositories.
 The number of opened pull requests whose latest activity (comment or update) is older than
-`ignore_last_days` should be under the specified maximum `max_opened_pulls`.
+`min_days_last_activity` should be under the specified maximum `max_opened_pulls`.
+All the opened pull-requests should have received activity in the last `max_days_last_activity` days.
 
 The number of total opened pull-requests along number of old ones is returned for each repository. Repositories with no opened pull-request are filtered.
 """
@@ -21,10 +22,10 @@ from poucave.utils import (
 
 logger = logging.getLogger(__name__)
 
-EXPOSED_PARAMETERS = ["ignore_last_days", "max_opened_pulls"]
+EXPOSED_PARAMETERS = ["max_days_last_activity", "max_opened_pulls"]
 
 
-async def pulls_info(session, repo: str) -> int:
+async def pulls_info(session, repo):
     token = os.getenv("GITHUB_TOKEN")
     headers = {
         "Accept": "application/vnd.github.v3+json",
@@ -59,7 +60,10 @@ async def pulls_info(session, repo: str) -> int:
 
 @retry_decorator
 async def run(
-    repositories: List[str], max_opened_pulls: int = 7, ignore_last_days: int = 7
+    repositories: List[str],
+    max_opened_pulls: int = 7,
+    min_days_last_activity: int = 7,
+    max_days_last_activity: int = 45,
 ) -> CheckResult:
     async with ClientSession() as session:
         futures = [pulls_info(session, repo) for repo in repositories]
@@ -70,7 +74,8 @@ async def run(
         for (repo, pulls) in zip(repositories, results):
             if len(pulls) == 0:
                 continue
-            old_pulls = [dt for dt in pulls if (utcnow() - dt).days > ignore_last_days]
+            age_pulls = [(utcnow() - dt).days for dt in pulls]
+            old_pulls = [age for age in age_pulls if age > min_days_last_activity]
             infos[repo] = {
                 "pulls": {
                     "old": len(old_pulls),
@@ -78,6 +83,8 @@ async def run(
                 }
             }
             if len(old_pulls) > max_opened_pulls:
+                success = False
+            if any([age > max_days_last_activity for age in age_pulls]):
                 success = False
 
         sorted_by_old = dict(
--- tests/checks/core/test_maintenance.py
+++ tests/checks/core/test_maintenance.py
@@ -23,7 +23,9 @@ async def test_positive(mock_aioresponses):
         payload=[],
     )
 
-    status, data = await run(repositories=["Kinto/kinto"])
+    fake_now = datetime(2019, 1, 27, 12, 0, 0, tzinfo=timezone.utc)
+    with mock.patch("checks.core.maintenance.utcnow", return_value=fake_now):
+        status, data = await run(repositories=["Kinto/kinto"])
 
     assert status is True
     assert data == {"Kinto/kinto": {"pulls": {"old": 1, "total": 1}}}
@@ -125,10 +127,49 @@ async def test_recent_pulls_dont_count(mock_aioresponses):
     )
 
     fake_now = datetime(2019, 1, 12, 12, 0, 0, tzinfo=timezone.utc)
-    with mock.patch(f"checks.core.maintenance.utcnow", return_value=fake_now):
+    with mock.patch("checks.core.maintenance.utcnow", return_value=fake_now):
         status, data = await run(
-            repositories=["Kinto/kinto-wizard"], max_opened_pulls=0, ignore_last_days=3
+            repositories=["Kinto/kinto-wizard"],
+            max_opened_pulls=0,
+            max_days_last_activity=3,
         )
 
     assert status is True
     assert data == {"Kinto/kinto-wizard": {"pulls": {"old": 0, "total": 2}}}
+
+
+async def test_fail_if_no_activity_for_days(mock_aioresponses):
+    mock_aioresponses.get(
+        "https://api.github.com/repos/Kinto/kinto/pulls?state=open",
+        status=200,
+        payload=[
+            {
+                "id": 615734086,
+                "draft": False,
+                "review_comments_url": "https://api.github.com/repos/Kinto/kinto/pulls/615734086/comments",
+                "updated_at": "2000-01-05T00:00:00Z",
+            },
+        ],
+    )
+    mock_aioresponses.get(
+        "https://api.github.com/repos/Kinto/kinto/pulls/615734086/comments?sort=updated_at&direction=desc",
+        status=200,
+        payload=[
+            {
+                "updated_at": "2019-01-09T13:59:35Z",
+            },
+            {
+                "updated_at": "1982-01-01T00:00:00Z",
+            },
+        ],
+    )
+    mock_aioresponses.get(
+        "https://api.github.com/repos/Kinto/kinto/pulls/615734087/comments?sort=updated_at&direction=desc",
+        status=200,
+        payload=[],
+    )
+
+    status, data = await run(repositories=["Kinto/kinto"])
+
+    assert status is False
+    assert data == {"Kinto/kinto": {"pulls": {"old": 1, "total": 1}}}
