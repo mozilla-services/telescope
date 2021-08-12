18d9d7834bfab9a9676d1f638aebe23828dc63e6
Mathieu Leplatre <mathieu@mozilla.com>
2021-08-12 14:22:01 +0200

    Add tests skeleton

---
+++ tests/checks/taskcluster/test_task_e2e.py
@@ -0,0 +1,22 @@
+async def test_positive_when_task_is_running():
+    pass
+
+
+async def test_positive_when_task_is_complete():
+    pass
+
+
+async def test_negative_if_running_for_too_long():
+    pass
+
+
+async def test_negative_if_artifact_unreachable():
+    pass
+
+
+async def test_negative_if_no_message_in_output():
+    pass
+
+
+async def test_negative_if_ran_for_too_long():
+    pass
