161279d7fd162a83df0edddd131ce1806225e10e
Mathieu Leplatre <mathieu@mozilla.com>
2021-08-30 18:13:23 +0200

    Ref #851: check latest indexed (part 1) (#871)

---
+++ checks/taskcluster/utils.py
@@ -0,0 +1,13 @@
+from poucave import config
+
+
+def options_from_params(root_url, client_id, access_token, certificate):
+    return {
+        "rootUrl": root_url,
+        "credentials": (
+            {"clientId": client_id, "accessToken": access_token}
+            if client_id and access_token
+            else {"certificate": certificate}
+        ),
+        "maxRetries": config.REQUESTS_MAX_RETRIES,
+    }
