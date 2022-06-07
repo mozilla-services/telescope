665b38478b2ab3b77a3c6b66e2cd5f5303e063d4
Mathieu Leplatre <mathieu@mozilla.com>
2022-06-07 23:10:36 +0200

    Add check to inspect total number of approvals in the last days (#1030)
    * Add check about total of approvals
    
    * Add unit tests
    
    * Update checks/remotesettings/total_approvals.py
    
    Co-authored-by: grahamalama <graham.beckley@gmail.com>
    
    * Graham review
    
    Co-authored-by: grahamalama <graham.beckley@gmail.com>

---
+++ checks/remotesettings/total_approvals.py
@@ -0,0 +1,85 @@
+"""
+Dummy-check to obtain information about the total number of approvals in the last days.
+
+For each day, the total number of approvals is returned, with details per collection.
+"""
+from collections import Counter
+from datetime import datetime, timezone
+
+from telescope.typings import CheckResult
+from telescope.utils import run_parallel, utcfromtimestamp, utcnow
+
+from .utils import KintoClient, fetch_signed_resources
+
+
+ONE_DAY_MSEC = 24 * 3600 * 1000
+
+DEFAULT_PLOT = "0.totals"
+
+
+async def get_approvals(client, bucket, min_timestamp, max_timestamp):
+    changes = await client.get_history(
+        bucket=bucket,
+        **{
+            "resource_name": "collection",
+            "action": "update",
+            "target.data.status": "to-sign",
+            "_since": min_timestamp,
+            "_before": max_timestamp,
+        },
+    )
+    by_collection = Counter(change["collection_id"] for change in changes)
+    return by_collection
+
+
+async def run(server: str, auth: str, period_days: int = 3) -> CheckResult:
+    # Compute the list of timestamps couples of the past full days,
+    # starting from today at midnight (last night).
+    today_00h00_timestamp = (
+        datetime.combine(utcnow(), datetime.min.time(), tzinfo=timezone.utc).timestamp()
+        * 1000
+    )
+    days_min_max = []
+    for iday in range(period_days):
+        iday_00h00_timestamp = today_00h00_timestamp - (iday + 1) * ONE_DAY_MSEC
+        iday_23h59_timestamp = today_00h00_timestamp - iday * ONE_DAY_MSEC
+        days_min_max.append((iday_00h00_timestamp, iday_23h59_timestamp))
+
+    # Get the list of bucket names used as source (eg. main-workspace, ...)
+    resources = await fetch_signed_resources(server, auth)
+    source_buckets = {r["source"]["bucket"] for r in resources}
+
+    # Prepare an array with information about each of the last days.
+    client = KintoClient(server_url=server, auth=auth)
+    days = []
+    for (min_day, max_day) in days_min_max:
+        day = {
+            "date": utcfromtimestamp(min_day).date().isoformat(),
+            "totals": 0,
+        }
+        # Approvals for each bucket on this day.
+        futures = [
+            get_approvals(client, bucket, min_day, max_day) for bucket in source_buckets
+        ]
+        results = await run_parallel(*futures)
+        approvals_per_bucket = zip(source_buckets, results)
+        # Sum the totals and show counters per collection on each day
+        for bucket, counter in approvals_per_bucket:
+            for cid, total in counter.items():
+                day[f"{bucket}/{cid}"] = total
+                day["totals"] += total
+        days.append(day)
+
+    # [
+    #   {
+    #     "date": "2022-05-17",
+    #     "totals": 112,
+    #     "main-workspace/nimbus-preview": 98,
+    #     "main-workspace/normandy-recipes-capabilities": 2,
+    #     "main-workspace/nimbus-mobile-experiments": 5,
+    #     "main-workspace/nimbus-desktop-experiments": 2,
+    #     "security-state-staging/cert-revocations": 4,
+    #     "security-state-staging/intermediates": 1
+    #   },
+    #   ...
+    return True, days
---
+++ tests/checks/remotesettings/test_total_approvals.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+from unittest import mock
+
+from checks.remotesettings.total_approvals import get_approvals, run
+from checks.remotesettings.utils import KintoClient
+from telescope.utils import utcnow
+from tests.utils import patch_async
+
+
+FAKE_AUTH = ""
+HISTORY_URL = "/buckets/{}/history"
+
+
+async def test_get_approvals(mock_responses):
+    server_url = "http://fake.local/v1"
+    history_url = server_url + HISTORY_URL.format("bid")
+    query_params = (
+        "?resource_name=collection&target.data.status=to-sign"
+        "&action=update&_since=42&_before=52"
+    )
+    mock_responses.get(
+        history_url + query_params,
+        payload={
+            "data": [
+                {"id": "abc", "collection_id": "cid"},
+                {"id": "efg", "collection_id": "cfr"},
+                {"id": "hij", "collection_id": "cfr"},
+            ]
+        },
+    )
+    client = KintoClient(server_url=server_url)
+
+    infos = await get_approvals(client, "bid", min_timestamp=42, max_timestamp=52)
+
+    assert infos == {
+        "cid": 1,
+        "cfr": 2,
+    }
+
+
+async def test_positive(mock_responses):
+    server_url = "http://fake.local/v1"
+    module = "checks.remotesettings.total_approvals"
+    resources = [
+        {
+            "last_modified": utcnow().timestamp() * 1000,
+            "source": {"bucket": "bid", "collection": None},
+        }
+    ]
+    totals = {
+        "cid": 10,
+        "cfr": 5,
+    }
+    with mock.patch(f"{module}.utcnow", return_value=datetime(1982, 5, 8)):
+        with patch_async(f"{module}.fetch_signed_resources", return_value=resources):
+            with patch_async(f"{module}.get_approvals", return_value=totals):
+
+                status, data = await run(server_url, FAKE_AUTH, period_days=1)
+
+    assert status is True
+    assert data == [{"date": "1982-05-07", "totals": 15, "bid/cid": 10, "bid/cfr": 5}]
