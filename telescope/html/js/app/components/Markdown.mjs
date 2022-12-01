0938f8789d7e78592e3633ac1742aac8a1d61490
Mathieu Leplatre <mathieu@mozilla.com>
2022-12-01 15:19:17 +0100

    Follow-up #1120: fix breakage of UI (#1134)

--- telescope/html/js/app/components/Markdown.mjs
+++ telescope/html/js/app/components/Markdown.mjs
@@ -2,7 +2,7 @@ import { html } from "../../htm_preact.mjs";
 
 const Markdown = ({ md }) => {
   const innerHTML = {
-    __html: marked(md),
+    __html: marked.parse(md),
   };
 
   return html`<div dangerouslySetInnerHTML="${innerHTML}" />`;
