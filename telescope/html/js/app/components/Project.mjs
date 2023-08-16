0db0ad101e109594488345f18c1509e3c70672e2
Mathieu Leplatre <mathieu@mozilla.com>
2023-08-16 17:36:34 +0200

    Fix #1259: add anchors to groups of checks

--- telescope/html/js/app/components/Project.mjs
+++ telescope/html/js/app/components/Project.mjs
@@ -55,12 +55,17 @@ export default class Project extends Component {
   }
 
   render({ name }) {
+    const slug = name.toLowerCase().replace(" ", "-");
     return html`
       <section class="project mt-3 pt-4">
         <div class="float-right mt-1 lh-1">${this.renderStatus()}</div>
         <h3 class="mb-4">
           <i class="fa fa-layer-group mr-2"></i>
-          <span class="project-name">${name}</span>
+          <a href="#project-${slug}" id="project-${slug}">
+            <span class="project-name">${name}</span>
+            ${" "}
+            <i class="fa fa-link"></i>
+          </a>
         </h3>
         <div class="project-cards mb-1">${this.renderChecks()}</div>
       </section>
