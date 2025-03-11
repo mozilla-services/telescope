89ee885e27769c115a17efab53be636ad8746c5d
Mathieu Leplatre <mathieu@mozilla.com>
2025-03-11 11:14:41 +0100

    Fix #1543: add check to verify age of resource (#1545)
    * Fix #1543: add check to verify age of resource
    
    * @say-yawn review

---
+++ checks/core/resource_age.py
@@ -0,0 +1,29 @@
+"""
+The age of the specified resource at URL should be less than the specified maximum.
+
+The age in hours is returned.
+"""
+
+from telescope.typings import CheckResult
+from telescope.utils import fetch_head, utcfromhttpdate, utcnow
+
+
+EXPOSED_PARAMETERS = ["url", "max_age_hours"]
+DEFAULT_PLOT = ".age_hours"
+
+
+async def run(url: str, max_age_hours: int) -> CheckResult:
+    _, headers = await fetch_head(url)
+
+    try:
+        last_modified = headers["Last-Modified"]
+    except KeyError:
+        return False, {"error": "Missing Last-Modified header"}
+    try:
+        last_modified_dt = utcfromhttpdate(last_modified)
+    except ValueError:
+        return False, {"error": f"Invalid Last-Modified header: {last_modified!r}"}
+
+    age_hours = int((utcnow() - last_modified_dt).total_seconds() / 3600)
+
+    return age_hours < max_age_hours, {"age_hours": age_hours}
---
+++ tests/checks/core/test_resource_age.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timezone
+from unittest import mock
+
+from checks.core.resource_age import run
+
+
+async def test_positive(mock_aioresponses):
+    url = "http://cdn.server/foo"
+    may8_http = "Mon, 08 May 1982 00:01:01 GMT"
+    fake_now = datetime(1982, 5, 8, 10, 0, 0).replace(tzinfo=timezone.utc)
+    mock_aioresponses.head(
+        url,
+        headers={"Last-Modified": may8_http},
+    )
+
+    with mock.patch("checks.core.resource_age.utcnow", return_value=fake_now):
+        status, data = await run(url, max_age_hours=10)
+
+    assert data == {"age_hours": 9}
+    assert status is True
+
+
+async def test_negative(mock_aioresponses):
+    url = "http://cdn.server/foo"
+    may8_http = "Mon, 08 May 1982 00:01:01 GMT"
+    fake_now = datetime(1982, 5, 8, 11, 0, 0).replace(tzinfo=timezone.utc)
+    mock_aioresponses.head(
+        url,
+        headers={"Last-Modified": may8_http},
+    )
+
+    with mock.patch("checks.core.resource_age.utcnow", return_value=fake_now):
+        status, data = await run(url, max_age_hours=10)
+
+    assert data == {"age_hours": 10}
+    assert status is False
+
+
+async def test_negative_missing(mock_aioresponses):
+    url = "http://cdn.server/foo"
+    mock_aioresponses.head(url)
+
+    status, data = await run(url, max_age_hours=10)
+
+    assert data == {"error": "Missing Last-Modified header"}
+    assert status is False
+
+
+async def test_negative_bad_format(mock_aioresponses):
+    url = "http://cdn.server/foo"
+    bad_format = "00:00:00 GMT"
+    mock_aioresponses.head(
+        url,
+        headers={"Last-Modified": bad_format},
+    )
+
+    status, data = await run(url, max_age_hours=10)
+
+    assert data == {"error": "Invalid Last-Modified header: '00:00:00 GMT'"}
+    assert status is False
