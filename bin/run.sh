a6d89214aff3b0b626ca5d124b3677b8888bc333
Mathieu Leplatre <mathieu@mozilla.com>
2025-08-05 11:07:28 +0200

    Pass prod args to all uv run commands

--- bin/run.sh
+++ bin/run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+export UV_NO_EDITABLE=1
+UV_PROD_ARGS="--no-dev --frozen"
+
 if [ $1 == "server" ]; then
-    exec uv run python -m telescope
+    exec uv run $UV_PROD_ARGS python -m telescope
 
 elif [ $1 == "check" ]; then
-    exec uv run python -m telescope $@
+    exec uv run $UV_PROD_ARGS python -m telescope $@
 
 elif [ $1 == "test" ]; then
-    uv install --group remotesettings --verbose
+    uv sync --group remotesettings --verbose
     uv run pytest tests
 
 else
-    exec uv run "$@"
+    exec uv run $UV_PROD_ARGS -- "$@"
 fi
