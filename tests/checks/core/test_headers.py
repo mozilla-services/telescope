7a68868d79e4fead35c55e34932505113b6abc57
Mathieu Leplatre <mathieu@mozilla.com>
2025-10-03 01:41:55 +0200

    Improve missing headers check

--- tests/checks/core/test_headers.py
+++ tests/checks/core/test_headers.py
@@ -17,6 +17,22 @@ async def test_positive(mock_aioresponses):
     assert data == {}
 
 
+async def test_positive_empty(mock_aioresponses):
+    url = "http://server.local"
+    mock_aioresponses.head(url, status=200, headers={"Content-Encoding": "gzip"})
+
+    status, data = await run(
+        [url],
+        request_headers={},
+        response_headers={
+            "Content-Encoding": "",
+        },
+    )
+
+    assert status is True
+    assert data == {}
+
+
 async def test_negative_missing(mock_aioresponses):
     url = "http://server.local"
     mock_aioresponses.head(url, status=200, headers={})
@@ -30,11 +46,7 @@ async def test_negative_missing(mock_aioresponses):
     )
 
     assert status is False
-    assert data == {
-        url: {
-            "Content-Encoding": "",
-        }
-    }
+    assert data == {url: {"missing": {"Content-Encoding": ""}}}
 
 
 async def test_negative_different(mock_aioresponses):
@@ -58,6 +70,6 @@ async def test_negative_different(mock_aioresponses):
     assert status is False
     assert data == {
         url: {
-            "Content-Encoding": "application/wasm",
+            "missing": {"Content-Encoding": "application/wasm"},
         }
     }
