34a67cdbe2b9bd9f350b6af9fe145c920910c6c7
Mathieu Leplatre <mathieu@mozilla.com>
2025-12-09 17:23:43 +0100

    Use Github token in reader source check

--- checks/remotesettings/git_reader_commit.py
+++ checks/remotesettings/git_reader_commit.py
@@ -4,6 +4,7 @@ The Remote Settings over git commit should be the latest commit of the specified
 The git reader commit and the repo latest commit details are returned.
 """
 
+from telescope import config
 from telescope.typings import CheckResult
 from telescope.utils import fetch_json, utcfromisoformat, utcnow
 
@@ -16,12 +17,22 @@ BRANCH = "v1/common"
 async def run(
     server: str, repo: str, branch: str = "main", lag_margin_seconds: int = 3600
 ) -> CheckResult:
+    # Latest Github commit.
+    headers = {
+        "Accept": "application/vnd.github.v3+json",
+    }
+    if config.GITHUB_TOKEN:
+        headers["Authorization"] = config.GITHUB_TOKEN
+    details = await fetch_json(
+        f"https://api.github.com/repos/{repo}/branches/{BRANCH}", headers=headers
+    )
+    if "commit" not in details:
+        return False, f"Could not fetch commit info from {details}"
+
     server_info = await fetch_json(server + "/")
     git_info = server_info["git"]["common"]
     source_commit = git_info["id"]
 
-    # Latest Github commit.
-    details = await fetch_json(f"https://api.github.com/repos/{repo}/branches/{BRANCH}")
     latest_sha = details["commit"]["sha"]
     latest_datetime = details["commit"]["commit"]["author"]["date"]
 
