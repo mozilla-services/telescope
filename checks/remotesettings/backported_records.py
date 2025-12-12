f6ef0d35784c01834e21bb944023c941023e9408
Mathieu Leplatre <mathieu@mozilla.com>
2025-12-12 14:25:33 +0100

    Reuse session in backported check

--- checks/remotesettings/backported_records.py
+++ checks/remotesettings/backported_records.py
@@ -12,6 +12,7 @@ from urllib.parse import parse_qs
 from kinto_http.utils import collection_diff
 
 from telescope.typings import CheckResult
+from telescope.utils import ClientSession
 
 from .utils import KintoClient, human_diff
 
@@ -22,35 +23,40 @@ EXPOSED_PARAMETERS = ["server", "max_lag_seconds"]
 async def run(
     server: str, backports: Dict[str, str], max_lag_seconds: int = 5 * 60
 ) -> CheckResult:
-    client = KintoClient(server_url=server)
+    async with ClientSession() as session:
+        client = KintoClient(server_url=server, session=session)
 
-    errors = []
-    for source, dest in backports.items():
-        # If source is filtered, then the check should take it into account.
-        filters: Dict[str, Any] = {}
-        if "?" in source:
-            source, qs = source.split("?")
-            filters = parse_qs(qs)
-            filters = {k: v[0] if len(v) == 1 else v for k, v in filters.items()}
+        errors = []
+        for source, dest in backports.items():
+            # If source is filtered, then the check should take it into account.
+            filters: Dict[str, Any] = {}
+            if "?" in source:
+                source, qs = source.split("?")
+                filters = parse_qs(qs)
+                filters = {k: v[0] if len(v) == 1 else v for k, v in filters.items()}
 
-        source_bid, source_cid = source.split("/")
-        dest_bid, dest_cid = dest.split("/")
+            source_bid, source_cid = source.split("/")
+            dest_bid, dest_cid = dest.split("/")
 
-        source_records = await client.get_records(
-            bucket=source_bid, collection=source_cid, params={**filters}
-        )
-        dest_records = await client.get_records(bucket=dest_bid, collection=dest_cid)
-        to_create, to_update, to_delete = collection_diff(source_records, dest_records)
-        if to_create or to_update or to_delete:
-            source_timestamp = await client.get_records_timestamp(
-                bucket=source_bid, collection=source_cid
+            source_records = await client.get_records(
+                bucket=source_bid, collection=source_cid, params={**filters}
             )
-            dest_timestamp = await client.get_records_timestamp(
+            dest_records = await client.get_records(
                 bucket=dest_bid, collection=dest_cid
             )
-            diff_millisecond = abs(int(source_timestamp) - int(dest_timestamp))
-            if (diff_millisecond / 1000) > max_lag_seconds:
-                details = human_diff(source, dest, to_create, to_update, to_delete)
-                errors.append(details)
+            to_create, to_update, to_delete = collection_diff(
+                source_records, dest_records
+            )
+            if to_create or to_update or to_delete:
+                source_timestamp = await client.get_records_timestamp(
+                    bucket=source_bid, collection=source_cid
+                )
+                dest_timestamp = await client.get_records_timestamp(
+                    bucket=dest_bid, collection=dest_cid
+                )
+                diff_millisecond = abs(int(source_timestamp) - int(dest_timestamp))
+                if (diff_millisecond / 1000) > max_lag_seconds:
+                    details = human_diff(source, dest, to_create, to_update, to_delete)
+                    errors.append(details)
 
-    return len(errors) == 0, errors
+        return len(errors) == 0, errors
