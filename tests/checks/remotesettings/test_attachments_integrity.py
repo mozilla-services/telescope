8146e0440eba6b9ea8e7ae78e63c4ced19514605
Mathieu Leplatre <mathieu@mozilla.com>
2025-12-22 16:49:38 +0100

    Split attachment integrity slices by attachment sizes (#1670)

--- tests/checks/remotesettings/test_attachments_integrity.py
+++ tests/checks/remotesettings/test_attachments_integrity.py
@@ -86,7 +86,7 @@ async def test_negative(mock_aioresponses, no_sleep):
                         "location": "file1.jpg",
                     },
                 },
-                {"id": "efg", "attachment": {"location": "missing.jpg"}},
+                {"id": "efg", "attachment": {"location": "missing.jpg", "size": 7}},
                 {
                     "id": "ijk",
                     "attachment": {"size": 10, "hash": "foo", "location": "file2.jpg"},
@@ -113,20 +113,20 @@ async def test_negative(mock_aioresponses, no_sleep):
     assert data == {
         "bad": [
             {
-                "error": "size differ (5!=7)",
-                "url": "http://cdn/file1.jpg",
-            },
-            {
-                "error": "Connection refused: GET http://cdn/missing.jpg",
-                "url": "http://cdn/missing.jpg",
+                "error": "timeout",
+                "url": "http://cdn/file3.jpg",
             },
             {
                 "error": "hash differ (bf2cb58a68f684d95a3b78ef8f661c9a4e5b09e82cc8f9cc88cce90528caeb27!=foo)",
                 "url": "http://cdn/file2.jpg",
             },
             {
-                "error": "timeout",
-                "url": "http://cdn/file3.jpg",
+                "error": "size differ (5!=7)",
+                "url": "http://cdn/file1.jpg",
+            },
+            {
+                "error": "Connection refused: GET http://cdn/missing.jpg",
+                "url": "http://cdn/missing.jpg",
             },
         ],
         "checked": 4,
@@ -169,7 +169,7 @@ async def test_urls_slicing(
         records_url,
         payload={
             "changes": [
-                {"id": f"id{i}", "attachment": {"location": f"file{i}.jpg"}}
+                {"id": f"id{i}", "attachment": {"location": f"file{i}.jpg", "size": 10}}
                 for i in range(100)
             ]
         },
@@ -183,3 +183,51 @@ async def test_urls_slicing(
     calls = mocked.call_args_list
     assert calls[0][0][0]["location"] == f"http://cdn/file{expected_lower}.jpg"
     assert calls[-1][0][0]["location"] == f"http://cdn/file{expected_upper}.jpg"
+
+
+async def test_urls_slicing_by_size(mock_aioresponses):
+    server_url = "http://fake.local/v1"
+    mock_aioresponses.get(
+        server_url + "/",
+        payload={"capabilities": {"attachments": {"base_url": "http://cdn/"}}},
+    )
+    changes_url = server_url + CHANGESET_URL.format("monitor", "changes")
+    mock_aioresponses.get(
+        changes_url,
+        payload={
+            "changes": [
+                {"id": "abc", "bucket": "bid", "collection": "cid", "last_modified": 42}
+            ]
+        },
+    )
+    records_url = server_url + CHANGESET_URL.format("bid", "cid") + "?_expected=42"
+    mock_aioresponses.get(
+        records_url,
+        payload={
+            "changes": [
+                {"id": "id-0", "attachment": {"location": "file-big.jpg", "size": 100}},
+                {
+                    "id": "id-1",
+                    "attachment": {"location": "file-small1.jpg", "size": 10},
+                },
+                {
+                    "id": "id-2",
+                    "attachment": {"location": "file-small2.jpg", "size": 10},
+                },
+                {
+                    "id": "id-3",
+                    "attachment": {"location": "file-small3.jpg", "size": 10},
+                },
+            ]
+        },
+    )
+
+    with mock.patch(
+        "checks.remotesettings.attachments_integrity.test_attachment"
+    ) as mocked:
+        mocked.return_value = {}, True
+        await run(server_url, slice_percent=(0, 50))
+    calls = mocked.call_args_list
+    # Only the big file should be in the first 50% slice.
+    assert len(calls) == 1
+    assert calls[0][0][0]["location"] == "http://cdn/file-big.jpg"
